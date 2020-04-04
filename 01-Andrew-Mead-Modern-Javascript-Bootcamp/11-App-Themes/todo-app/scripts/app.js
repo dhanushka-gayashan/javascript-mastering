10ca271c6f9371420eb696ae0308e79e66b5e894
dhanushka gayashan <dhanukdg.soft@gmail.com>
2020-04-04 23:24:18 +1300

    add app themes

---
+++ 01-Andrew-Mead-Modern-Javascript-Bootcamp/11-App-Themes/todo-app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+let todos = getSavedTodos()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', (event) => {
+    filters.searchText = event.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    const text = event.target.elements.text.value.trim()    
+    if (text.length > 0) {        
+        todos.push({
+            id: uuidv4(),
+            text,
+            completed: false
+        })
+        saveTodos(todos)
+        renderTodos(todos, filters)               
+    }
+    event.target.elements.text.value = '' 
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (event) => {
+    filters.hideCompleted = event.target.checked
+    renderTodos(todos, filters)
+})
